b56176d543411a163a8b3a557a39b30a21fbac76
Jahmal <Jahmal23@gmail.com>
2015-07-04 12:58:52 -0400

    test cleanup

--- ExerciseTests/MathFunTests.cs
+++ ExerciseTests/MathFunTests.cs
@@ -11,55 +11,52 @@ namespace ExerciseTests
         public void ShouldPerformSimpleCalculation()
         {
             int result = MathFun.PerformPolish("+-678");
-            Assert.AreEqual(result, 7, "Simple Calc Failed");
+            Assert.AreEqual(7, result, "Simple Calc Failed");
         }
 
         [TestMethod]
         public void ShouldPerformLongerCalculation()
         {
             int result = MathFun.PerformPolish("x+--59381");
-            Assert.AreEqual(result, 1, "Longer Calc Failed");
+            Assert.AreEqual(1, result, "Longer Calc Failed");
         }
 
         [TestMethod]
         public void ShouldPerformSuperLongCalculation()
         {
             int result = MathFun.PerformPolish("x++-x+-12345678");
-            Assert.AreEqual(result, 128, "Super Long Calc Failed");
+            Assert.AreEqual(128, result, "Super Long Calc Failed");
         }
 
         [TestMethod]
         public void ShouldNotPerformUnbalancedCalculation()
         {
             int result = MathFun.PerformPolish("+-45");
-            Assert.AreEqual(result, -1, "Unbalanced Calc Failed");
+            Assert.AreEqual(-1, result, "Unbalanced Calc Failed");
 
             int result2 = MathFun.PerformPolish("+--1");
-            Assert.AreEqual(result2, -1, "Unbalanced Calc 2 Failed");
+            Assert.AreEqual(-1, result2, "Unbalanced Calc 2 Failed");
 
             int result3 = MathFun.PerformPolish("+-1111111111111");
-            Assert.AreEqual(result3, -1, "Unbalanced Calc 3 Failed");
+            Assert.AreEqual(-1, result3, "Unbalanced Calc 3 Failed");
         }
 
         [TestMethod]
         public void ShouldNotPerformNullCalculation()
         {
             int result = MathFun.PerformPolish(null);
-            Assert.AreEqual(result, -1, "Null Calc Failed");
+            Assert.AreEqual(-1, result, "Null Calc Failed");
         }
 
         [TestMethod]
         public void ShouldNotPerformEmptyCalculation()
         {
             int result = MathFun.PerformPolish(string.Empty);
-            Assert.AreEqual(result, -1, "Empty Calc Failed");
+            Assert.AreEqual(-1, result, "Empty Calc Failed");
 
             int result2 = MathFun.PerformPolish("      ");
-            Assert.AreEqual(result, -1, "Empty Calc 2 Failed");
+            Assert.AreEqual(-1, result, "Empty Calc 2 Failed");
         }
-
-
-
 
     }
 }
--- ExerciseTests/StringFunTests.cs
+++ ExerciseTests/StringFunTests.cs
@@ -11,30 +11,30 @@ namespace ExerciseTests
         [TestMethod]
         public void ShouldGetOccurrences()
         {
-            Assert.AreEqual(StringFun.GetOccurrencesEachChar("Jahmal"), "J:1a:2h:1m:1l:1", "Test for Jahmal Failed");
-            Assert.AreEqual(StringFun.GetOccurrencesEachChar("Natasha"), "N:1a:3t:1s:1h:1", "Test for Natasha Failed");
+            Assert.AreEqual("J:1a:2h:1m:1l:1",StringFun.GetOccurrencesEachChar("Jahmal"), "Test for Jahmal Failed");
+            Assert.AreEqual("N:1a:3t:1s:1h:1",StringFun.GetOccurrencesEachChar("Natasha"), "Test for Natasha Failed");
         }
 
         [TestMethod]
         public void ShouldGetOccurrencesOnBadInput()
         {
-            Assert.AreEqual(StringFun.GetOccurrencesEachChar(""), "", "Empty String Test Failed");
+            Assert.AreEqual("",StringFun.GetOccurrencesEachChar(""),"Empty String Test Failed");
             Assert.AreEqual(StringFun.GetOccurrencesEachChar(null), "", "Null String Test Failed");
         }
 
         [TestMethod]
         public void ShouldRemoveOccurrences()
         {
-            Assert.AreEqual(StringFun.RemoveOccurrences("Jahmal",'a'), "Jhml", "Test for Jahmal Failed");
-            Assert.AreEqual(StringFun.RemoveOccurrences("Natasha",'N'), "atasha","Test for Natasha Failed");
+            Assert.AreEqual("Jhml",StringFun.RemoveOccurrences("Jahmal",'a'), "Test for Jahmal Failed");
+            Assert.AreEqual("atasha",StringFun.RemoveOccurrences("Natasha",'N'),"Test for Natasha Failed");
         }
 
         [TestMethod]
         public void ShouldRemoveOccurrencesOnBadInput()
         {
-            Assert.AreEqual(StringFun.RemoveOccurrences("", 'a'), "", "Empty String Test Failed");
+            Assert.AreEqual("",StringFun.RemoveOccurrences("", 'a'), "Empty String Test Failed");
             Assert.AreEqual(StringFun.RemoveOccurrences("Joe", 'z'), "Joe", "Missing char Test Failed");
-            Assert.AreEqual(StringFun.RemoveOccurrences(null, 'z'), "", "Null String Test Failed");
+            Assert.AreEqual("",StringFun.RemoveOccurrences(null, 'z'), "Null String Test Failed");
         }
 
     }
