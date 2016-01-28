4b405970d35a0d0deb2b6a738d350666a685fa0c
Jahmal <Jahmal23@gmail.com>
2016-01-28 18:09:43 -0500

    async fun

---
+++ ExerciseTests/AsyncFunTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercises;
+
+namespace ExerciseTests
+{
+    [TestClass]
+    public class AsyncFunTests
+    {
+        [TestMethod]
+        public void ShouldRun()
+        {
+            var af = new AsyncFun();
+
+            af.GetPeople();
+        }
+    }
+}
---
+++ Exercises/AsyncFun.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    public class AsyncFun
+    {
+        public async void GetPeople()
+        {
+            Console.WriteLine("Do something else");
+
+            Task<List<string>> list = FakeSearch();
+
+
+            Console.WriteLine("Do something else");
+
+            var results = await list;
+
+
+            Console.WriteLine("go it");
+        }
+
+        public async Task<List<string>> FakeSearch()
+        {
+            await Task.Delay(3000);
+
+            return new List<string>() { "foo", "man", "choo" };
+        }
+    }
+
+
+}
